e422957bc10adbec9f42008cb2c45291f926174f
Francisco <coheteomega@gmail.com>
2023-10-06 13:30:04 +0200

    Mejoras a los botones

--- infoCuenta.js
+++ infoCuenta.js
@@ -6,28 +6,35 @@ class banco{
 
     ingresar(cantidad) {
         const numero=/^[0-9]+$/
+        var mensaje=document.getElementById('msg')
         if (numero.test(cantidad)) {
             this.saldo+=cantidad
-            
+            mensaje.textContent='Se ha ingresado correctamente, tienes '+this.saldo+'€'
+            mensaje.style.color='green'
         }else{
-            console.log('Debe escribir una cantidad en números')
+            mensaje.textContent='No se ha ingresado correctamente'
+            mensaje.style.color='red'
         }
     }
 
     retirar(cantidad){
         const numero=/^[0-9]+$/
-
+        var mensaje=document.getElementById('msg')
         if (numero.test(cantidad)) {
             if (this.saldo<=0){
-                msg.innerHTML='La cuenta esta a 0'
+                mensaje.textContent='La cuenta esta a 0'
+                mensaje.style.color='red'
             }else if (this.saldo-cantidad<0){
-                msg.innerHTML('No puedes retirar tanto dinero, tienes '+this.saldo+'€ en la cuenta')
+                mensaje.textContent='No puedes retirar tanto dinero, tienes '+this.saldo+'€ en la cuenta'
+                mensaje.style.color='red'
             }else{
                 this.saldo-=cantidad
-                msg.innerHTML='Se ha retirado correctamente el dinero'
+                mensaje.textContent='Se ha retirado correctamente el dinero, tienes '+this.saldo+'€'
+                mensaje.style.color='green'
             }
         }else{
-            msg.innerHTML='Desbes escribir una cantidad en números'
+            mensaje.textContent='Desbes escribir una cantidad en números'
+            mensaje.style.color='red'
         }
     }
 }
@@ -39,13 +46,14 @@ const botonRetirar=document.getElementById('saldoRetirar')
 const botonIngresar=document.getElementById('saldoIngresar')
 
 botonRetirar.addEventListener('click', function(event){
-
-var mensaje=document.getElementById('msg')
-mensaje.textContent('hola')
-mensaje.style.color='green'
-
-
-
+    const retiradoInput = document.getElementById('retirado')
+    const cantidadRetirar = Number(retiradoInput.value)
+    cuenta.retirar(cantidadRetirar)
 
 })
-botonIngresar.addEventListener('click', () => cuenta.ingresar('ingresado'))
+
+botonIngresar.addEventListener('click',function(event){
+    const ingresadoInput=document.getElementById('ingresado')
+    const cantidadIngresar=Number(ingresadoInput.value)
+    cuenta.ingresar(cantidadIngresar)
+})
