7b3d9818c898fd391432268b570fce6228c7d39e
badr <badrhamidou@gmail.com>
2023-10-07 12:50:55 +0200

    fix some functions and refactor de class Banco and Tarjeta

--- table.js
+++ table.js
@@ -2,10 +2,27 @@ class Banco {
   constructor(iban, saldo) {
     this.iban = iban;
     this.saldo = saldo;
+    this.tarjeta = [];
   }
 
   getIban() {
     return this.iban;
+  }
+
+  getSaldo() {
+    return this.saldo;
+  }
+
+  addTarjeta(tar) {
+    this.tarjeta.push(tar);
+  }
+
+  getSizeTarjeta() {
+    let count = 0;
+    this.tarjeta.forEach((element) => {
+      count++;
+    });
+    return count;
   }
 }
 
@@ -39,11 +56,9 @@ let tarjeta = new Tarjeta(" t1 ", "No");
 let t2 = new Tarjeta(" t2 ", 123, "Si");
 let t3 = new Tarjeta(" t3 ", 123, "Si");
 
-let v = [
-  [cuenta, tarjeta],
-  [c2, t2],
-  [c3, t3],
-];
+cuenta.addTarjeta(tarjeta);
+cuenta.addTarjeta(t2);
+cuenta.addTarjeta(t3);
 
 function tableCreate() {
   const tbl = document.getElementById("table");
@@ -54,65 +69,96 @@ function tableCreate() {
   let tr = tbl.insertRow();
   let td = tr.insertCell();
 
-  v.forEach((arr) => {
+  let i = 0;
+
+  while (i < cuenta.tarjeta.length) {
+    let j = 0;
     tr = tbl.insertRow();
-    arr.forEach((value) => {
+    while (j < 1) {
       td = tr.insertCell();
+      td.appendChild(document.createTextNode(cuenta.iban));
 
-      if (value instanceof Banco) {
-        td.appendChild(document.createTextNode(value.getIban()));
-      }
+      td = tr.insertCell();
+      td.appendChild(document.createTextNode(cuenta.tarjeta[i].tarjeta));
 
-      if (value instanceof Tarjeta) {
-        td.appendChild(document.createTextNode(value.getTarjeta()));
-      }
+      td = tr.insertCell();
+      td.appendChild(document.createTextNode(cuenta.tarjeta[i].getActiva()));
 
-      if (value instanceof Tarjeta) {
-        td = tr.insertCell();
-        td.appendChild(document.createTextNode(value.getActiva()));
-      }
-    });
-  });
+      j++;
+    }
+
+    i++;
+  }
+}
+
+let data = localStorage.getItem("cuenta");
+
+if (data != null) {
+  let parseObjCuenta = JSON.parse(data);
+  cuenta = new Banco(parseObjCuenta.iban, parseObjCuenta.saldo);
+  let i = 0;
+
+  while (i < parseObjCuenta.tarjeta.length) {
+    var t = new Tarjeta(
+      parseObjCuenta.tarjeta[i].tarjeta,
+      parseObjCuenta.tarjeta[i].cvv,
+      parseObjCuenta.tarjeta[i].activa
+    );
+
+    cuenta.addTarjeta(t);
+    i++;
+  }
+} else {
+  let cuenta = new Banco("ES21 1465 0100 72 2030876293", 500);
+  let tarjetita = new Tarjeta("test", false);
+  cuenta.tarjeta.push(tarjetita);
+
+  // console.log(parseJSON)
 }
 
 function guardar() {
-  let nombre = document.getElementById("nombre").value
-  let cvv = document.getElementById("cvv").value
-  let check = document.getElementById("check").checked
+  console.log(cuenta);
 
-  let message = ""
+  let nombre = document.getElementById("nombre").value;
+  let cvv = document.getElementById("cvv").value;
+  let check = document.getElementById("check").checked;
 
+  let message = "";
 
-    if(checkTarjeta(nombre)){
-        if(checkCVV(cvv)){
-            let tarjeta = new Tarjeta(nombre, cvv, check);
-            let arr = [cuenta, tarjeta];
-            v.push(arr);
-            tableCreate();
-        }else if(cvv.length>3){
-            message += "<li>El CVV solo admite hasta 3 dígitos</li>"
-        }else{
-            message += "<li>El CVV está vacío</li>"
-        }
-    }else {
-        message += "<li>La tarjeta tiene un mal formato</li>"
+  if (checkTarjeta(nombre)) {
+    if (checkCVV(cvv)) {
+      let tarjeta = new Tarjeta(nombre, cvv, check);
+      cuenta.tarjeta.push(tarjeta);
+      tableCreate();
+    } else if (cvv.length > 3) {
+      message += "<li>El CVV solo admite hasta 3 dígitos</li>";
+    } else {
+      message += "<li>El CVV está vacío</li>";
     }
-  
-  document.getElementById("msg").innerHTML  = `<ul>${message}</ul>`
+  } else {
+    message += "<li>La tarjeta tiene un mal formato</li>";
+  }
+
+  document.getElementById("msg").innerHTML = `<ul>${message}</ul>`;
+
+
+  let parseJSON = JSON.stringify(cuenta);
+  localStorage.setItem("cuenta", parseJSON);
+
 }
 
 function checkTarjeta(str) {
-  let cadena = /^[0-9]+$/;
+  let cadena = /^[0-9]{4}\s[0-9]{5}\s[0-9]{6}$/;
 
   let rtnBool = cadena.test(str);
 
   return rtnBool;
 }
 
-function checkCVV(str){
-    let cadena = /^[0-9]{3}$/;
+function checkCVV(str) {
+  let cadena = /^[0-9]{3}$/;
 
-    let rtnBool = cadena.test(str);
-  
-    return rtnBool;
+  let rtnBool = cadena.test(str);
+
+  return rtnBool;
 }
